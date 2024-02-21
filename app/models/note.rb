cf8dba189cdf14fda30f804539a4d677954cc249
Gonzalo Parada <gonzalo.parada@widergy.com>
2024-02-21 20:19:02 -0300

    feat: add word_count, content_length and validation for content length

--- app/models/note.rb
+++ app/models/note.rb
@@ -13,11 +13,27 @@
 class Note < ApplicationRecord
   enum note_type: { review: 0, critique: 1 }
   validates :title, :content, :note_type, :user_id, presence: true
+  validate :review_must_be_short, on: :create
 
   belongs_to :user
   has_one :utility, through: :user
 
   def word_count
     content.scan(/\p{alpha}+|\d+(?:\.\d+)*/).length
+  end
+
+  def content_length
+    count = word_count
+    return 'short' if count <= utility.short_content
+    return 'medium' if count <= utility.medium_content
+    'long'
+  end
+
+  def review_must_be_short
+    errors.add(:base, 'review incorrecta') unless valid_content_count?
+  end
+
+  def valid_content_count?
+    note_type == 'critique' || (note_type == 'review' && word_count <= utility.short_content)
   end
 end
