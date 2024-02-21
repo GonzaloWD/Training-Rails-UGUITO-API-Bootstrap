6f2a4db1185150038e0d338b293ce16367e3d875
Gonzalo Parada <gonzalo.parada@widergy.com>
2024-02-21 20:49:00 -0300

    fix: uncomment

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Admin User
-# FactoryBot.create(:admin_user, email: 'admin@example.com', password: 'password',
-#                                password_confirmation: 'password')
+FactoryBot.create(:admin_user, email: 'admin@example.com', password: 'password',
+                               password_confirmation: 'password')
 
-# # Utilities
-# north_utility = FactoryBot.create(:north_utility, code: 1)
-# south_utility = FactoryBot.create(:south_utility, code: 2)
+# Utilities
+north_utility = FactoryBot.create(:north_utility, code: 1)
+south_utility = FactoryBot.create(:south_utility, code: 2)
 
-# # Users
-# FactoryBot.create_list(:user, 20, utility: north_utility,
-#                                   password: '12345678', password_confirmation: '12345678')
-# FactoryBot.create_list(:user, 20, utility: south_utility,
-#                                   password: '12345678', password_confirmation: '12345678')
+# Users
+FactoryBot.create_list(:user, 20, utility: north_utility,
+                                  password: '12345678', password_confirmation: '12345678')
+FactoryBot.create_list(:user, 20, utility: south_utility,
+                                  password: '12345678', password_confirmation: '12345678')
 
-# FactoryBot.create(:user, utility: south_utility, email: 'test_south@widergy.com',
-#                          password: '12345678', password_confirmation: '12345678')
+FactoryBot.create(:user, utility: south_utility, email: 'test_south@widergy.com',
+                         password: '12345678', password_confirmation: '12345678')
 
-# FactoryBot.create(:user, utility: north_utility, email: 'test_north@widergy.com',
-#                          password: '12345678', password_confirmation: '12345678')
+FactoryBot.create(:user, utility: north_utility, email: 'test_north@widergy.com',
+                         password: '12345678', password_confirmation: '12345678')
 users = User.all
-# users.find_each do |user|
-#   random_books_amount = [1, 2, 3].sample
-#   FactoryBot.create_list(:book, random_books_amount, user: user, utility: user.utility)
-# end
+users.find_each do |user|
+  random_books_amount = [1, 2, 3].sample
+  FactoryBot.create_list(:book, random_books_amount, user: user, utility: user.utility)
+end
 
 # Notes
 users.find_each.with_index do |user, index|
