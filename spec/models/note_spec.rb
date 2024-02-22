e0927eca07916f8183d037ccb5f175252270d154
Gonzalo Parada <gonzalo.parada@widergy.com>
2024-02-22 10:52:41 -0300

    fix: refactor test with context

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -18,100 +18,132 @@ RSpec.describe Note, type: :model do
   end
 
   describe '#word_count' do
-    it 'returns the number of words in the content' do
-      subject.content = 'Esto es una prueba de siete palabras'
-      expect(subject.word_count).to eq(7)
+    context 'with generic input' do
+      it 'returns the number of words' do
+        subject.content = 'Esto es una prueba de siete palabras'
+        expect(subject.word_count).to eq(7)
+      end
     end
 
-    it 'correctly handles punctuation marks and spaces' do
-      subject.content = '¡Hola, hermoso mundo! Tiene seis palabras.'
-      expect(subject.word_count).to eq(6)
+    context 'with punctuation marks and spaces' do
+      it 'returns correct number of words' do
+        subject.content = '¡Hola, hermoso mundo! Tiene seis palabras.'
+        expect(subject.word_count).to eq(6)
+      end
     end
 
-    it 'handle numbers and words' do
-      subject.content = '12345 prueba de 4 numeros'
-      expect(subject.word_count).to eq(5)
+    context 'with numbers and words' do
+      it 'returns the number of words' do
+        subject.content = '12345 prueba de 4 numeros'
+        expect(subject.word_count).to eq(5)
+      end
     end
 
-    it 'correctly handles empty content' do
-      subject.content = ''
-      expect(subject.word_count).to eq(0)
-    end
-  end
-
-  describe '#content_length with north utility' do
-    subject(:note) { create(:note, user: user) }
-
-    let(:north_utility) { create(:north_utility, code: 1) }
-    let(:user) { create(:user, utility: north_utility) }
-
-    it 'returns short for content words count equal or less than 50' do
-      subject.content = 'rep ' * 50
-      expect(subject.content_length).to eq('short')
-    end
-
-    it 'returns medium for content words count  equal or less than 100' do
-      subject.content = 'rep ' * 100
-      expect(subject.content_length).to eq('medium')
-    end
-
-    it 'returns long for content words count greater than 100' do
-      subject.content = 'rep ' * 120
-      expect(subject.content_length).to eq('long')
+    context 'with empty content' do
+      it 'return 0' do
+        subject.content = ''
+        expect(subject.word_count).to eq(0)
+      end
     end
   end
 
-  describe '#content_length with south utility' do
-    subject(:note) { create(:note, user: user) }
+  describe '#content_length' do
+    context 'with north utility' do
+      subject(:note) { create(:note, user: user) }
 
-    let(:south_utility) { create(:south_utility, code: 2) }
-    let(:user) { create(:user, utility: south_utility) }
+      let(:north_utility) { create(:north_utility, code: 1) }
+      let(:user) { create(:user, utility: north_utility) }
 
-    it 'returns short for content words count equal or less than 50' do
-      subject.content = 'rep ' * 60
-      expect(subject.content_length).to eq('short')
+      context 'with words count equal or less than 50' do
+        it 'returns short' do
+          subject.content = 'rep ' * 50
+          expect(subject.content_length).to eq('short')
+        end
+      end
+
+      context 'with content words count equal or less than 100' do
+        it 'returns medium' do
+          subject.content = 'rep ' * 100
+          expect(subject.content_length).to eq('medium')
+        end
+      end
+
+      context 'with content words count greater than 100' do
+        it 'returns long' do
+          subject.content = 'rep ' * 120
+          expect(subject.content_length).to eq('long')
+        end
+      end
     end
 
-    it 'returns medium for content words count  equal or less than 100' do
-      subject.content = 'rep ' * 120
-      expect(subject.content_length).to eq('medium')
-    end
+    context 'with south utility' do
+      subject(:note) { create(:note, user: user) }
 
-    it 'returns long for content words count greater than 100' do
-      subject.content = 'rep ' * 130
-      expect(subject.content_length).to eq('long')
+      let(:south_utility) { create(:south_utility, code: 2) }
+      let(:user) { create(:user, utility: south_utility) }
+
+      context 'with words count equal or less than 60' do
+        it 'returns short' do
+          subject.content = 'rep ' * 60
+          expect(subject.content_length).to eq('short')
+        end
+      end
+
+      context 'with content words count equal or less than 120' do
+        it 'returns medium' do
+          subject.content = 'rep ' * 120
+          expect(subject.content_length).to eq('medium')
+        end
+      end
+
+      context 'with content words count greater than 120' do
+        it 'returns long' do
+          subject.content = 'rep ' * 130
+          expect(subject.content_length).to eq('long')
+        end
+      end
     end
   end
 
-  describe '#valid_content_count? for type review' do
-    subject(:note_type_review) do
-      FactoryBot.create(:note, :review)
+  describe '#valid_content_count?' do
+    context 'with type review' do
+      subject(:note_type_review) do
+        FactoryBot.create(:note, :review)
+      end
+
+      context 'when review word_count greater than 60' do
+        it 'returns false' do
+          subject.content = subject.content = 'rep ' * 70
+          expect(subject.valid_content_count?).to eq(false)
+        end
+      end
+
+      context 'when review word_count equal or less than 60' do
+        it 'returns true' do
+          subject.content = subject.content = 'rep ' * 40
+          expect(subject.valid_content_count?).to eq(true)
+        end
+      end
     end
 
-    it 'returns false if review word_count greater than 60' do
-      subject.content = subject.content = 'rep ' * 70
-      expect(subject.valid_content_count?).to eq(false)
-    end
+    context 'with type critique' do
+      subject(:note_type_critique) do
+        FactoryBot.create(:note, :critique)
+      end
 
-    it 'returns true if review word_count equal or less than 60' do
-      subject.content = subject.content = 'rep ' * 40
-      expect(subject.valid_content_count?).to eq(true)
-    end
-  end
+      context 'when critique word_count greater than 60' do
+        it 'returns true' do
+          subject.content = subject.content = 'rep ' * 70
+          expect(subject.valid_content_count?).to eq(true)
+        end
+      end
 
-  describe '#valid_content_count? for type critique' do
-    subject(:note_type_critique) do
-      FactoryBot.create(:note, :critique)
-    end
-
-    it 'returns true if critique word_count greater than 60' do
-      subject.content = subject.content = 'rep ' * 70
-      expect(subject.valid_content_count?).to eq(true)
-    end
-
-    it 'returns true if review word_count equal or less 60' do
-      subject.content = subject.content = 'rep ' * 40
-      expect(subject.valid_content_count?).to eq(true)
+      context 'when review word_count equal or less 60' do
+        it 'returns true' do
+          subject.content = subject.content = 'rep ' * 40
+          expect(subject.valid_content_count?).to eq(true)
+        end
+      end
     end
   end
 end
