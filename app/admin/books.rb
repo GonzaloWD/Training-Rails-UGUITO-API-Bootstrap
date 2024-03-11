08b302a8f6f65295a6cfa25a73fb51a9dffdc7fe
Gonzalo Parada <160155300+GonzaloWD@users.noreply.github.com>
2024-03-11 19:20:44 -0300

    feat: [TD-273] active admin books view (#9)
    * feat: add word_count method and test
    
    * feat: create seeds for Note and refactor note factory
    
    * feat: add word_count, content_length and validation for content length
    
    * feat: add test for note
    
    * fix: uncomment
    
    * feat: add message error with i18n for review content validation
    
    * fix: validation
    
    * fix: refactor test with context
    
    * feat: add note controller with router and serializers
    
    * feat: add test for note controller
    
    * fix: add status 406 when type is not accepted
    
    * fix: removing valid_type_param check in notes_filtered
    
    * fix: refactor note controller
    
    * fix: adding scope to notes
    
    * fix: remove comparation json to json
    
    * fix: adding routes annotate
    
    * feat: add devise jwt authentication
    
    * fix: add note with no current user
    
    * fix: rubocop offences
    
    * feat: add create service to note controller
    
    * feat: add error handlers and error render methods
    
    * feat: add test create
    
    * feat: add test
    
    * fix: robocop offence
    
    * feat: test for validate content length
    
    * fix: test invalid content length
    
    * fix: removing unnecessary methods
    
    * fix: random missing param for note test
    
    * feat: test for validate register is created in db for notes
    
    * fix: seed correction
    
    * fix: remove blank space
    
    * feat: api integration for notes
    
    * fix: robocop offence
    
    * fix: correction in map_notes for North utility
    
    * feat: add cassettes and retrieve notes worker spec
    
    * feat: annotate notes/async in routes
    
    * feat: add async service test
    
    * feat: book active admin
    
    * feat: update in routes
    
    * fix: adding associations for utility and user in book view for active admin
    
    * fix: robocop offence
    
    * fix: add request mapper

---
+++ app/admin/books.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register Book do
+  filter :title
+  filter :genre
+  filter :publisher
+  filter :year
+  filter :utility
+  filter :created_at
+  filter :updated_at
+
+  includes :utility, :user
+
+  permit_params :title, :publisher, :year, :genre, :author, :image, :user_id, :utility_id
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :author
+    column :genre
+    column :utility
+    column :publisher
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :publisher
+      f.input :year
+      f.input :genre
+      f.input :author
+      f.input :image
+      f.input :image
+      f.input :user_id, label: 'User', as: :select, collection: User.all.map { |user|
+        ["#{user.first_name} #{user.last_name}", user.id]
+      }
+      f.input :utility_id, label: 'Utility', as: :select, collection: Utility.all.map { |utility|
+        [utility.name, utility.id]
+      }
+    end
+    f.actions
+  end
+end
